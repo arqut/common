61aad41fa8b6e4c3e7206cb482271fee5b77e229
Tuan Phan <tphanx@gmail.com>
2025-01-13 19:38:40 +0700

    add account public_id

--- auth/middleware.go
+++ auth/middleware.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -34,22 +35,9 @@ func remoteMiddleware(extractTokens ...string) fiber.Handler {
 			return api.ErrorUnauthorizedResp(ctx, "Missing auth token or apikey")
 		}
 
-		act := &Account{}
-		err := cache.GetObj(token, act)
-
-		if err != nil || act.ID == 0 {
-			system.Logger.Errorf("[common/auth] caching error: %v", err)
-			resp := &AuthAccountResponse{}
-			err := http.Get(system.Env("AUTH_API")+"/auth/account", resp, "Authorization", "Bearer "+token)
-			if err != nil {
-				return api.ErrorUnauthorizedResp(ctx, err.Error())
-			}
-			if !resp.Success {
-				return api.ErrorUnauthorizedResp(ctx, resp.Error.Message)
-			}
-			act = resp.Data
-			duration, _ := time.ParseDuration(system.Env("AUTH_CACHE_DURATION", "1h"))
-			cache.SetObj(token, act, duration)
+		act, err := RemoteAccount(token)
+		if err != nil {
+			return api.ErrorUnauthorizedResp(ctx, err.Error())
 		}
 
 		ctx.Locals("authToken", token)
@@ -59,4 +47,27 @@ func remoteMiddleware(extractTokens ...string) fiber.Handler {
 
 		return ctx.Next()
 	}
+}
+
+func RemoteAccount(token string) (act *Account, err error) {
+	act = &Account{}
+	err = cache.GetObj(token, act)
+
+	if err != nil || act.ID == 0 {
+		system.Logger.Errorf("[common/auth] caching error: %v", err)
+		err = nil
+		resp := &AuthAccountResponse{}
+		err := http.Get(system.Env("AUTH_API")+"/auth/account", resp, "Authorization", "Bearer "+token)
+		if err != nil {
+			return nil, err
+		}
+		if !resp.Success {
+			return nil, errors.New(resp.Error.Message)
+		}
+		act = resp.Data
+		duration, _ := time.ParseDuration(system.Env("AUTH_CACHE_DURATION", "1h"))
+		cache.SetObj(token, act, duration)
+	}
+
+	return act, err
 }
