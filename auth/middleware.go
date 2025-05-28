5677615b593f9539477117aeadd75c2346abc6d7
Tuan Phan <tphanx@gmail.com>
2025-05-29 06:42:46 +0700

    middleware supports finalized option

--- auth/middleware.go
+++ auth/middleware.go
@@ -2,71 +2,91 @@ package auth
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/arqut/common/api"
 	"github.com/arqut/common/cache"
 	"github.com/arqut/common/http"
 	"github.com/arqut/common/system"
+	"github.com/arqut/common/types"
 	"github.com/arqut/common/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
-func RemoteAuthMiddleware() fiber.Handler {
+func RemoteAuthMiddleware(finalized ...bool) fiber.Handler {
 	extractTokens := "header:Authorization,query:auth_token"
 	return remoteMiddleware(extractTokens)
 }
 
-func RemoteAPIKeyMiddleware() fiber.Handler {
+func RemoteAPIKeyMiddleware(finalized ...bool) fiber.Handler {
 	extractTokens := "header:Authorization,query:apikey"
 	return remoteMiddleware(extractTokens)
 }
 
 // RemoteMiddleware accept both auth_token or apikey
-func RemoteMiddleware() fiber.Handler {
+func RemoteMiddleware(finalized ...bool) fiber.Handler {
 	extractTokens := "header:Authorization,query:auth_token,query:apikey"
-	return remoteMiddleware(extractTokens)
+	return remoteMiddleware(extractTokens, finalized...)
 }
 
-func remoteMiddleware(extractTokens ...string) fiber.Handler {
+func remoteMiddleware(extractToken string, finalized ...bool) fiber.Handler {
+	isFinalized := true
+	if len(finalized) > 0 {
+		isFinalized = finalized[0]
+	}
+
 	return func(ctx *fiber.Ctx) error {
-		token := ExtractToken(ctx, extractTokens...)
+		token := ExtractToken(ctx, extractToken)
 		if token == "" {
-			return api.ErrorUnauthorizedResp(ctx, "Missing auth token or apikey")
+			if isFinalized {
+				return api.ErrorUnauthorizedResp(ctx, "Missing auth token or apikey")
+			} else {
+				return errors.New("missing auth token or apikey")
+			}
 		}
 
-		act, err := RemoteAccount(token)
+		acc, err := RemoteAccount(token)
 		if err != nil {
-			return api.ErrorUnauthorizedResp(ctx, err.Error())
+			if isFinalized {
+				return api.ErrorUnauthorizedResp(ctx, err.Error())
+			} else {
+				return err
+			}
 		}
 
+		// keep token
+		acc.Meta = &types.Map{
+			"token": token,
+		}
+
+		ctx.Locals("account", acc)
 		ctx.Locals("authToken", token)
-		ctx.Locals("account", act)
-		ctx.Locals("uiID", act.ID)
-		ctx.Locals("usID", fmt.Sprintf("%d", act.ID))
 
 		return ctx.Next()
 	}
 }
 
-func RemoteAccount(token string) (act *AuthTokenData, err error) {
-	act = &AuthTokenData{}
-	err = cache.GetObj(token, act)
+func RemoteAccount(token string) (acc *AuthTokenData, err error) {
+	acc = &AuthTokenData{}
+	err = cache.GetObj(token, acc)
 
-	if err != nil || act.ID == 0 {
+	if err != nil || acc.ID == 0 {
 		err = nil
-		resp := &AuthValidateResponse{}
-		err := http.Get(system.Env("AUTH_API")+"/auth/validate", resp, "Authorization", "Bearer "+token)
+		res := &AuthValidateResponse{}
+		err := http.Get(system.Env("AUTH_API")+"/auth/validate", res, "Authorization", "Bearer "+token)
 		if err != nil {
 			return nil, err
 		}
-		if !resp.Success {
-			return nil, errors.New(resp.Error.Message)
+		if !res.Success {
+			return nil, errors.New(res.Error.Message)
 		}
-		act = resp.Data
+
+		acc = res.Data
+		acc.Meta = &types.Map{
+			"token": token,
+		}
 		duration, _ := utils.ParseDuration(system.Env("AUTH_CACHE_DURATION", "1h"))
-		cache.SetObj(token, act, duration)
+		cache.SetObj(token, acc, duration)
 	}
 
-	return act, err
+	return acc, err
 }
